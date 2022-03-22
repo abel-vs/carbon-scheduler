03d4691505b63e6f18a67561f2d3aa68fbcddf3c
Wander <wandersiemers@me.com>
2022-03-22 17:29:18 +0100

    More work on scheduling: now can schedule repeated and one-off jobs

--- src/scheduler.py
+++ src/scheduler.py
@@ -1,16 +1,12 @@
+import subprocess
 from datetime import datetime
 import argparse
 import time
 import os
 import sys
-from crontab import CronTab
+from crontab import CronTab, CronItem
 
 from apscheduler.schedulers.background import BackgroundScheduler
-
-
-def train_model():
-    print('dask train_model! The time is: %s' % datetime.now())
-
 
 if __name__ == '__main__':
     
@@ -25,12 +21,29 @@ if __name__ == '__main__':
                        type=str,
                        help='the Python filename of the job to schedule')
 
+    my_parser.add_argument('--repeat',
+                       metavar='repeat',
+                       type=str,
+                       help='a cron expression or a natural language expression describing the \
+                       repetition schedule of the job')
+
+    my_parser.add_argument('--at',
+                       metavar='at',
+                       type=str,
+                       help='a natural language string describing when to schedule the task')
+
     my_parser.add_argument('--span',
                        metavar='span',
                        type=str,
                        help='the estimated span, or duration, of the job, in seconds',
                        required=False)
 
+    my_parser.add_argument('--not-before',
+                       metavar='not_before',
+                       type=str,
+                       help='minimum number of seconds that have to pass before starting task',
+                       required=False)
+    
     my_parser.add_argument('--deadline',
                        metavar='deadline',
                        type=str,
@@ -40,9 +53,32 @@ if __name__ == '__main__':
     # Execute the parse_args() method
     args = my_parser.parse_args()
 
+    #print(args.job)
     cron = CronTab(user='wander')
-    job = cron.new(command=f'python {os.path.abspath(args.job)} >> ~/Desktop/output2.txt 2>&1')
+    for job in cron:
+        print(job)
+
+    output_file = "~/Desktop/output.txt 2>&1"
+    if args.repeat is not None:
+        # we have a repeating job, schedule using `cron`
+        job = cron.new(command=f"python {os.path.abspath(args.job)} >> {output_file}")
+        job.minute.every(1)
+
+        item = CronItem.from_line(f"* * * * * echo ehhhh >> {output_file}", cron=cron)
+        #cron.append(item)
+        #cron.write()
+        print("scheduled repeating job")
+    elif args.at is not None:
+        # we have a one-off job, schedule using `at`
+        cmd = f"python {os.path.abspath(args.job)} >> {output_file} | at {args.at} >> /dev/null 2>&1"
+        subprocess.run(cmd, shell=True) # TODO: remove shell=True
+        print("scheduled one-off job")
+    else:
+        print("error: one of the following arguments is required: repeat, at")
     
-    job.minute.every(1)
-    cron.write()
+
+    
+    
+
+    
 
