f2dec82cc01a37047e95d5ed5363a309a8dd8321
Wander <wandersiemers@me.com>
2022-03-24 11:57:29 +0100

    Allow specifying both relative and absolute times using --at. Renamed and removed some metavars where appropriate Renamed --not_before to --delay after Florentin's feedback

--- src/scheduler.py
+++ src/scheduler.py
@@ -13,50 +13,45 @@ from offline_model import OfflineModel
 def parse_args():
      # Create the parser
 
-    parser = argparse.ArgumentParser(description='Schedule Python tasks')
+    parser = argparse.ArgumentParser(description='Carbon Scheduler v0.1')
 
     # Add the arguments
     parser.add_argument('job',
-                       metavar='job',
                        type=str,
                        help='the Python filename of the job to schedule')
 
     parser.add_argument('--repeat',
-                       metavar='repeat',
+                       metavar='SCHEDULE',
                        type=str,
                        help='a cron expression or a natural language expression describing the \
                        repetition schedule of the job')
 
     parser.add_argument('--at',
-                       metavar='at',
+                       metavar='DATE_SPECIFICATION',
                        type=str,
-                       help='a natural language string describing when to schedule the task')
+                       help='a string describing when to schedule the task')
 
-    parser.add_argument('-t', 
+    parser.add_argument('-t',  # TODO: implement this
                         action='store_true', # so default = False
                         help='pass this flag if you set --at and you want to specify time \
                              absolutely instead of relatively') 
 
     parser.add_argument('--span', '--duration',
-                       metavar='span',
                        type=str,
                        help='the estimated span, or duration, of the job, in seconds',
                        required=False)
 
-    parser.add_argument('--not-before',
-                       metavar='not_before',
+    parser.add_argument('--delay',
                        type=str,
                        help='minimum number of seconds that have to pass before starting task',
                        required=False)
     
     parser.add_argument('--deadline',
-                       metavar='deadline',
                        type=str,
                        help='the estimated deadline of the job, in seconds from now',
                        required=False)
 
     parser.add_argument('--output', '--o',
-                       metavar='output',
                        type=str,
                        help='the path to the output file',
                        required=False)
@@ -92,8 +87,8 @@ if __name__ == '__main__':
         deadline = datetime.datetime.now() + datetime.timedelta(seconds=args.deadline)
 
     start = None
-    if args.not_before is not None:
-        start = datetime.datetime.now() + datetime.timedelta(seconds=args.not_before)
+    if args.delay is not None:
+        start = datetime.datetime.now() + datetime.timedelta(seconds=args.delay)
 
     model = OfflineModel('NL')
     task = task.Task(duration, deadline, start)
@@ -103,21 +98,24 @@ if __name__ == '__main__':
 
     if args.repeat is not None:
         # we have a repeating job, schedule using `cron`
-        #job = cron.new(command=f'python {os.path.abspath(args.job)} >> {output_file}')
-        #job.minute.every(1)
 
-        item = CronItem.from_line(f'{args.repeat} python {os.path.abspath(args.job)} >> \
-            {output_file}', cron=cron)
+        # extra parentheses around the strings to get a multiline string without whitespace
+        # see: https://stackoverflow.com/a/10660443
+        item = CronItem.from_line((f'{args.repeat} python {os.path.abspath(args.job)} >> '
+                                    f'{output_file}'), cron=cron)
         cron.append(item)
         cron.write()
         print('scheduled repeating job')
     elif args.at is not None:
         # we have a one-off job, schedule using `at`
-        time_str = new_start.strftime('%Y%m%d%H%M') # [[CC]YY]MMDDhhmm
-        print(time_str)
-        print('new start: ' + str(new_start))
-        cmd = f'python {os.path.abspath(args.job)} >> {output_file}\
-             | at -t {time_str} >> /dev/null 2>&1'
+        time_str = new_start.strftime('%Y%m%d%H%M') # at's format:[[CC]YY]MMDDhhmm
+        at_options = ""
+        at_time = time_str
+        if args.t is True:
+            at_options = '-t'
+            at_time = args.at
+        cmd = (f'python {os.path.abspath(args.job)} >> {output_file} '
+                f'| at {at_options} {at_time} >> /dev/null') #2>&1')
         subprocess.run(cmd, shell=True) # TODO: remove shell=True
         print('scheduled one-off job')
     else:
