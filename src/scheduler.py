127b95dccc21b24a375ced01568d3b99b37c6c10
Wander <wandersiemers@me.com>
2022-03-24 09:56:10 +0100

    Stop hardcoding output directory, create task properly, pass country to model, actually call cron, use single quote consistently

--- src/scheduler.py
+++ src/scheduler.py
@@ -4,6 +4,7 @@ import argparse
 import os
 import sys
 from crontab import CronTab, CronItem
+import task
 
 from apscheduler.schedulers.background import BackgroundScheduler
 
@@ -51,70 +52,59 @@ if __name__ == '__main__':
                        help='the estimated deadline of the job, in seconds from now',
                        required=False)
 
-    my_parser.add_argument('--list', '--l',
-                       metavar='deadline',
+    my_parser.add_argument('--output', 'o',
+                       metavar='output',
                        type=str,
-                       help='the estimated deadline of the job, in seconds from now',
+                       help='the path to the output file',
                        required=False)
-    
-    # atq
-    # crontab -l
 
-    # python src/scheduler -l 
-    # overview of scheduled jobs
-    # REPEATING
-    # * * * * * myfile.py (next run: 23 mar 2022 22:05)
-    # * * * * * myotherfile.py (next run: 24 mar 2022 22:05)
-    # NON-REPEATING
-    # myfile.py (runs: 23 mar 2022 22:05)
-
-
-    # Execute the parse_args() method
     args = my_parser.parse_args()
 
-    #print(args.job)
     cron = CronTab(user='wander')
     for job in cron:
         print(job)
 
-    output_file = "~/Desktop/output.txt 2>&1" # TODO: don't hardcode
-    model = OfflineModel()
+    output_file = '~/Desktop/output.txt'
+    if args.output is not None:
+        output_file = args.output
+    output_file.append(' 2>&1')
 
-    duration = 10 # in seconds
+    duration = datetime.timedelta(seconds=10) # in seconds
     if args.span is not None:
-        duration = args.span
+        duration = datetime.timedelta(seconds=args.span)
     
     deadline = datetime.datetime.now() + datetime.timedelta(days=7)
     if args.deadline is not None:
         deadline = datetime.datetime.now() + datetime.timedelta(seconds=args.deadline)
 
-    new_start = model.process_concurrently([(duration, deadline)])[0]
+    start = None
+    if args.not_before is not None:
+        start = datetime.datetime.now() + datetime.timedelta(seconds=args.not_before)
 
+    model = OfflineModel('NL')
+    task = task.Task(duration, deadline, start)
+    new_start = model.process_concurrently([task])[0]
+
+    print(new_start)
 
     if args.repeat is not None:
         # we have a repeating job, schedule using `cron`
-        #job = cron.new(command=f"python {os.path.abspath(args.job)} >> {output_file}")
+        #job = cron.new(command=f'python {os.path.abspath(args.job)} >> {output_file}')
         #job.minute.every(1)
 
-        item = CronItem.from_line(f"* * * * * echo ehhhh >> {output_file}", cron=cron)
+        item = CronItem.from_line(f'{args.repeat} python {os.path.abspath(args.job)} >> \
+            {output_file}', cron=cron)
         cron.append(item)
         cron.write()
-        print("scheduled repeating job")
+        print('scheduled repeating job')
     elif args.at is not None:
         # we have a one-off job, schedule using `at`
-        time_str = new_start.strftime("%Y%m%d%H%M") # [[CC]YY]MMDDhhmm
+        time_str = new_start.strftime('%Y%m%d%H%M') # [[CC]YY]MMDDhhmm
 
-        print(time_str)
-        cmd = f"python {os.path.abspath(args.job)} >> {output_file}\
-             | at {args.at} >> /dev/null"# 2>&1" # fix: use new deadline
+        print('new start: ' + str(new_start))
+        cmd = f'python {os.path.abspath(args.job)} >> {output_file}\
+             | at {args.at} >> /dev/null 2>&1'
         subprocess.run(cmd, shell=True) # TODO: remove shell=True
-        print("scheduled one-off job")
+        print('scheduled one-off job')
     else:
-        print("error: one of the following arguments is required: repeat, at")
-    
-
-    
-    
-
-    
-
+        print('error: one of the following arguments is required: repeat, at')
