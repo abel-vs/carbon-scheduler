790bacc5a41cd76738d4b81db6897a17bb4f2188
Wander <wandersiemers@me.com>
2022-04-05 21:02:10 +0200

    Bump version again

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="carbon-aware-scheduler",
-    version="1.0.1",
+    version="1.0.2",
     description="Carbon-minimizing task scheduler for Unix systems",
     long_description=README,
     long_description_content_type="text/markdown",
